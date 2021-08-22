6115ae13316e1662bd80daab0fe172e137e3781e
abrah <abrah@DESKTOP-HEDUBV3>
2021-08-22 18:01:39 +0300

    Proje dosyası ekle.

---
+++ GameProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer { Id = 1, BirthYear = 1985, FirstName = "enes", LastName = "özdemir", IdentityNumber = 12345 });
+        }
+    }
+}
